03cf57f684d5c41d280d493a8e2fd48c25524a24
raghuvd <meraghudy@gmail.com>
2017-10-02 19:50:47 +0530

    change to master

--- code/shell1.sh
+++ code/shell1.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/bash....
 clear
 echo "Good morning, world."
 
 echo "hello, $USER. I wish to list some files of yours"
-echo "listing files in the current directory, $PWD"
-ls  # list files
+
 
